bda723701b664e01ef68c8a0c05da168b9f20c76
iugal <glaguardia23@gmail.com>
2024-03-10 09:25:24 +0100

    adding all_zeros testbench

--- README.md
+++ README.md
@@ -5,13 +5,21 @@ open_project project_reti_logiche.xpr
 launch_simulation -mode post-synthesis -type functional
 
 set_property top project_tb_109 [get_filesets sim_1]
-set_property top_lib xil_defaultlib [get_filesets sim_1]
-update_compile_order -fileset sim_1
 
 check:
-258
-262
-276
+
+364
+
+
+
+996-1000
+
+
+
+
+
+
+
 
 
 
--- test_bench_bot.py
+++ test_bench_bot.py
@@ -1,23 +1,12 @@
 import pyautogui
 import time
 time.sleep(5)
-for i in range(305,1000):
+for i in range(996,1000):
     pyautogui.write(f"set_property top project_tb_{i} [get_filesets sim_1]")
     time.sleep(2)
     pyautogui.press("enter")
-    pyautogui.write("set_property top_lib xil_defaultlib [get_filesets sim_1]")
-    time.sleep(2)
-    pyautogui.press("enter")
-    pyautogui.write("update_compile_order -fileset sim_1")
-    time.sleep(2)
-    pyautogui.press("enter")
-    time.sleep(15)
     pyautogui.write("launch_simulation -mode post-synthesis -type functional ")
     time.sleep(1)
     pyautogui.press("enter") 
-    time.sleep(3)
+    time.sleep(15)
     #pyautogui.screenshot().save(f"C:\\Users\\glagu\\Desktop\\università\\PROGETTO RETI LOGICHE\\peponzo\\test_result\\test_result_{i}.jpeg")
-
-
-
-
